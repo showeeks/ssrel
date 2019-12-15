de53b2d2489cb9b11ac6f6f2f3f06f01a19bf9af
pdeantihuman <pdeantihuman@gmail.com>
2019-12-15 20:48:13 +0800

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+// import axios from 'axios'
+
+const subscribe = () => {
+    // axios.post("")
+}
 
 const App: React.FC = () => {
     return (
         <div>
+            <p><button onClick={() => subscribe()}>request</button></p>
             <p>Hello world!</p>
         </div>
     )
