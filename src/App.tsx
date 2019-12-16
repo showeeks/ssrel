71c16c14303b14c8049e512f1bd053e5a80fe5c8
pdeantihuman <pdeantihuman@gmail.com>
2019-12-16 22:48:35 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React, {useState} from 'react'
 import SSRConfig from "./model/SSRConfig";
-import {Layout, Menu, Breadcrumb, Button} from "antd";
+import {Layout, Menu, Breadcrumb, Button, Table, Divider, Tag, Checkbox} from "antd";
 
-const {Header, Content, Footer} = Layout
+const {Header, Content, Footer} = Layout;
 
 const electron = window.require("electron");
 const {send, sendSync} = electron.ipcRenderer;
@@ -13,7 +13,11 @@ function subscribe(setList) {
     console.log("try to setLists");
     // send("sub", "ping");
     let sendSync1 = sendSync("sub", "ping");
-    console.log(sendSync1)
+    console.log(sendSync1);
+    sendSync1 = sendSync1.map((data) => {
+        data.key = data.remarks;
+        return data
+    });
     setList(sendSync1)
     // electron.ipcRenderer.on("pub", (event, arg) => {
     //     console.log(arg)
@@ -22,20 +26,42 @@ function subscribe(setList) {
 
 function ListSSRConfig() {
     const [list, setList] = useState<SSRConfig[]>(new Array<SSRConfig>());
-    const listItems = list.map((con: SSRConfig) => {
-        return (<li key={con.remarks}>
-            {con.remarks} - {con.method} - {con.server}
-        </li>)
-    });
+    // const listItems = list.map((con: SSRConfig) => {
+    //     return (<li key={con.remarks}>
+    //         {con.remarks} - {con.method} - {con.server}
+    //     </li>)
+    // });
     return (
         <div>
             <p>
                 <Button onClick={() => subscribe(setList)}>fetch all</Button>
             </p>
             <p>Hello world!</p>
-            <ul>
-                {listItems}
-            </ul>
+            <Table columns={[
+                {
+                    title: "名称",
+                    dataIndex: "remarks",
+                },
+                {
+                    title: "IP",
+                    dataIndex: "server"
+                },
+                {
+                    title: "端口",
+                    dataIndex: "server_port"
+                }, {
+                    title: "分组",
+                    dataIndex: "group"
+                }, {
+                    title: "启用",
+                    dataIndex: "enable",
+                    key: "enable",
+                    render: (text) => {
+                        return <Checkbox checked={text} onClick={() => {}}/>
+                    }
+                }
+            ]} dataSource={list}>
+            </Table>
         </div>
     )
 }
@@ -43,30 +69,29 @@ function ListSSRConfig() {
 function MainLayout() {
     return (<Layout className="layout">
         <Header>
-            <div className="logo" />
+            <div className="logo"/>
             <Menu
                 theme="dark"
                 mode="horizontal"
                 defaultSelectedKeys={['2']}
-                style={{ lineHeight: '64px' }}
+                style={{lineHeight: '64px'}}
             >
                 <Menu.Item key="1">nav 1</Menu.Item>
                 <Menu.Item key="2">nav 2</Menu.Item>
                 <Menu.Item key="3">nav 3</Menu.Item>
             </Menu>
         </Header>
-        <Content style={{ padding: '0 50px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
+        <Content style={{padding: '0 50px'}}>
+            <Breadcrumb style={{margin: '16px 0'}}>
                 <Breadcrumb.Item>Home</Breadcrumb.Item>
                 <Breadcrumb.Item>List</Breadcrumb.Item>
                 <Breadcrumb.Item>App</Breadcrumb.Item>
             </Breadcrumb>
-            <div style={{ background: '#fff', padding: 24, minHeight: 280 }}><ListSSRConfig/></div>
+            <div style={{background: '#fff', padding: 24, minHeight: 280}}><ListSSRConfig/></div>
         </Content>
-        <Footer style={{ textAlign: 'center' }}>pdeantihuman ©2019 </Footer>
+        <Footer style={{textAlign: 'center'}}>pdeantihuman ©2019 </Footer>
     </Layout>)
 }
-
 
 
 const App: React.FC = () => {
